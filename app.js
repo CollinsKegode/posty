48172d552ea3937019934414f1315624ca0bfa33
CollinsKegode <collinskegode@gmail.com>
2023-02-01 23:30:57 +0300

    Add signup form errors to check password mismarch and if user exists

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from "express";
 import mysql from "mysql"
 
 const app = express()
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'posty'
+})
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -48,7 +54,39 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword
     }
 
-    console.log(user)
+    if (user.password === user.confirmPassword) {
+        // check if user exists
+        let sql = 'SELECT * FROM user WHERE email = ?'
+        connection.query(
+            sql,
+            [user.email],
+            (error, results) => {
+                if (results.length > 0) {
+                    // user exists
+                    let error = true
+                    let message = 'Account already exists with the email provided.'
+                    res.render('signup', {user, error, message})
+                } else {
+                    // create user
+                    let sql = 'INSERT INTO user (usename, email, password) VALUES (?,?,?)'
+                    connection.query(
+                        sql,
+                        [user.fullname, user.email, user.password],
+                        (errors, results) => {
+                            console.log('user successfully created');
+                        }
+                    )
+                }
+                
+            }
+        )
+       
+    } else {
+        // passwords do not match
+        let error = true
+        let message = 'PasswordS Mismarch!'
+        res.render('signup', {user, error, message})
+    }
 })
 
 app.listen(4000, () => {
